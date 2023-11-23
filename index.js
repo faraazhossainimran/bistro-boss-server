d404eacaa28667f5674d28a421aefc3bd10a97ec
Dev Enthusiastic <faraazhossainimran@gmail.com>
2023-11-24 03:55:52 +0600

    jwt added with admin security

--- index.js
+++ index.js
@@ -32,15 +32,55 @@ async function run() {
     app.post('/jwt', async(req, res)=> {
       const user = req.body
       console.log(user);
-      console.log(req.headers);
+
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: '1h'})
         res.send({token})
       })
-    
+    // middlewares
+    const verifyToken = (req, res, next) => {
+      console.log('headers from verifytoken', req.headers.authorization);
+      if(!req.headers.authorization){
+        return res.status(401).send({message: 'forbidden access'})
+      }
+      const token = req.headers.authorization.split(' ')[1]
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=> {
+        if(err){
+          return res.status(401).send({message: 'forbidden access'})
+        }
+        req.decoded = decoded;
+        next();
+      })
+      // next();
+    }
+    // user verify admin after verifyToken
+    const verifyAdmin = async(req, res, next)=> {
+      const email = req.decoded.email;
+      const query = {email: email}
+      const user = await userCollection.findOne(query)
+      const isAdmin = user?.role === 'admin';
+      if(!isAdmin){
+        return res.status(403).send({message: 'forbidden access'})
+      }
+      next()
+    }
     // user related api
+    app.get('/users/admin/:email',verifyToken, async(req, res)=> {
+      const email = req.params.email;
+      if(email !== req.decoded.email){
+        return res.status(403).send({message: 'unauthorized access'})
+      }
+      const query = {email: email}
+      const user = await userCollection.findOne(query)
+      let admin = false;
+      if(user){
+        admin = user?.role === 'admin'
+      }
+      res.send({admin})
+    })
     app.post('/users', async(req, res)=> {
       const user = req.body;
+
       // insert email if user doesn't exists
       // you can do this many ways. (1. email unique, 2. upsert 3. simple checking in database)
       const query = {email: user.email}
@@ -52,7 +92,8 @@ async function run() {
       res.send(result)
     })
 
-    app.get('/users', async(req, res)=> {
+    app.get('/users',verifyToken, verifyAdmin, async(req, res)=> {
+
       const result = await userCollection.find().toArray();
       res.send(result)
     })
