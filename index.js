f944074b8121513e677ae4d1e688954428342263
Dev Enthusiastic <faraazhossainimran@gmail.com>
2023-11-23 23:28:42 +0600

    jwt added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 // middleware
@@ -27,14 +28,55 @@ async function run() {
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewsCollection = client.db("bistroDb").collection("reviews");
     const cartCollection = client.db("bistroDb").collection("carts");
-
+    // jwt related api
+    app.post('/jwt', async(req, res)=> {
+      const user = req.body
+      console.log(user);
+      console.log(req.headers);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1h'})
+        res.send({token})
+      })
+    
     // user related api
     app.post('/users', async(req, res)=> {
       const user = req.body;
+      // insert email if user doesn't exists
+      // you can do this many ways. (1. email unique, 2. upsert 3. simple checking in database)
+      const query = {email: user.email}
+      const existingUser = await userCollection.findOne(query)
+      if(existingUser){
+        return res.send({message: 'user already exists', insertedId: null})
+      }
       const result = await userCollection.insertOne(user)
       res.send(result)
     })
+
+    app.get('/users', async(req, res)=> {
+      const result = await userCollection.find().toArray();
+      res.send(result)
+    })
+    app.delete('/users/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query)
+      res.send(result)
+
+    })
+    app.patch('/users/admin/:id', async(req, res)=> {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc)
+      res.send(result)
+    })
+// menu related apis
     app.get("/menu", async (req, res) => {
+
       const result = await menuCollection.find().toArray();
       res.send(result);
     });
